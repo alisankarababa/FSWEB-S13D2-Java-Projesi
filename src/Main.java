66a11a5d46b1c7a8ca8f1950c0a1776be1ba1831
Alişan <alisankarababa@gmail.com>
2023-11-14 11:32:34 +0300

    add isPalindrome and tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("isPalindrome Tests");
+        System.out.println(isPalindrome(-1221)); //true
+        System.out.println(isPalindrome(707)); //true
+        System.out.println(isPalindrome(11212)); //false
+
+        System.out.println("-----------------------------------------------");
+
+    }
+
+    public static boolean isPalindrome(int num) {
+        num = Math.abs(num);
+        String strNum = Integer.toString(num);
+
+        for (int s = 0, e = strNum.length() - 1; s < e ; ++s, --e) {
+
+            if(strNum.charAt(s) != strNum.charAt(e))
+                return false;
+        }
+
+        return true;
     }
 }
